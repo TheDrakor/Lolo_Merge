7fd33130d47d1abc188c970714dfd9dcbc012462
Lolo <chimichuymx@gmail.com>
2022-07-11 08:51:24 -0700

    Se ha modificado test.py con nuevo codigo

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
-n = input("Introduce tu nombre: ")
+from email.utils import parsedate_to_datetime
+
+
+name = input("\nIntroduce tu nombre: ")
+age = input("\nIntroduce tu edad: ")
+pasatiempo = input("\nTu pasatiempo favorito: ")
 
 while True:
-    print(f"\nHola {n}, es un gusto saludarte!\n")
+    print(f"\nHola {name}, es un gusto saludarte!\n")
+    print(f"Tu nombre es {name}, tienes {age} años y te gusta jugar {pasatiempo}")
     break
